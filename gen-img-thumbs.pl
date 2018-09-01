97737cd6f6c9ce7c13e0c164227084237843e55b
Andrew Proper <andrewrproper@gmail.com>
2018-09-01 19:17:35 -0400

    add docs and cleanup output: gen-img-thumbs.pl

--- gen-img-thumbs.pl
+++ gen-img-thumbs.pl
@@ -1,8 +1,24 @@
 #!/usr/bin/perl
 
+
+=head1 SYNOPSIS
+
+  gen-img-thumbs.pl [options]
+
+  generates thumbnail images for images under specific paths
+
+	options:
+	  --debug    # print debug output
+
+
 =head1 CHANGES
 
    - 2018-09-01 - Created by Andrew R. Proper
+
+=head1 LIMITATIONS
+
+Maybe it would be better to write this in Ruby, since Jekyll uses Ruby? Then
+this project would not require any Perl.
 
 =head1 WARNINGS
 
@@ -31,45 +47,89 @@ use warnings;
 use FindBin;
 use File::Find;
 use Carp;
+use Getopt::Long;
 use feature 'say';
 
-my $debug = 1;
 
-my @target_paths = (
-	$FindBin::Bin.'/images/art/',
-	$FindBin::Bin.'/images/software/',
+my $debug = 0;
+GetOptions(
+	'debug'	=> \$debug,
 );
-my $thumb_size = '250x250';
-my $is_jpeg_suffix_re = qr/.+\.jpg$/;
-my $is_thumb_re = qr/\-thumb/;
 
-# search down through the subfolders of these paths, for files/dirs
-File::Find::find( 
-	sub {
-		my $parent_dir 	= $File::Find::dir;
-		my $full_fn 		= $File::Find::name;
-		my $rel_fn 			= $_;
+main();
 
-		if ( 
-			-f $full_fn &&  # if $full_fn points to file (not a dir)
-			$full_fn =~ $is_jpeg_suffix_re && # if it has a jpeg suffix
-			$full_fn !~ $is_thumb_re  # if it does not match a thumb filename
-		) {
-			my $thumb_fn = $full_fn;
-			$thumb_fn =~ s/(\.[^\.]+)$/-thumb$1/;
-			# create an ImageMagick command-line command
-			z:\personal\
-			my $cmd = qq(magick "$full_fn" ).
-				qq(-thumbnail "$thumb_size^" ).
-				qq(-gravity center ).
-				qq(-extent "$thumb_size" ).
-				qq("$thumb_fn" );
-			run_cmd( $cmd );
-		}
+sub main {
 
-	},
-	@target_paths,
-);
+	# BEGIN - settings
+
+	# make target paths relative to this program's location
+	my @target_paths = (
+		$FindBin::Bin.'/images/art/',
+		$FindBin::Bin.'/images/software/',
+	);
+
+	my $thumb_size = '250x250';
+	my $is_jpeg_suffix_re = qr/.+\.jpg$/;
+	my $is_thumb_re = qr/\-thumb/;
+
+	# END - settings
+
+	say "";
+	say "generating thumbnails for images in paths:";
+	say "  $_" for @target_paths;
+
+	my $processed_count = 0;
+
+	# search down through the subfolders of these paths, for files/dirs
+	File::Find::find(  # see: perldoc File::Find
+		sub {
+			my $parent_dir 	= $File::Find::dir;
+			my $full_fn 		= $File::Find::name;
+			my $rel_fn 			= $_; # not used yet but may become useful
+
+			if ( 
+				# if $full_fn points to file (not a dir)
+				-f $full_fn &&  
+
+				# if it has a jpeg suffix
+				$full_fn =~ $is_jpeg_suffix_re && 
+
+				# if it does not match a thumb filename
+				$full_fn !~ $is_thumb_re  
+			) {
+
+				# make a thumbnail filename, based on the source filename
+				my $thumb_fn = $full_fn;
+				$thumb_fn =~ s/(\.[^\.]+)$/-thumb$1/; # add a string just before the '.jpg' portion
+
+				# create an ImageMagick command-line command
+				my $cmd = qq(magick ). 
+					# source image filename
+					qq("$full_fn" ).
+					# scale down to have smallest side fit this geometry
+					qq(-thumbnail "$thumb_size^" ). 
+					# keep image centered when scaling down
+					qq(-gravity center ).
+					# trim image down to this geometry, after scale down by -thumbnail
+					qq(-extent "$thumb_size" ).  
+					# output image filename
+					qq("$thumb_fn" );
+
+				run_cmd( $cmd );
+				$processed_count++;
+			}
+
+		},
+		@target_paths,
+	);
+
+
+	say "";
+	say "generated $processed_count thumbnail images";
+	say "";
+
+}
+
 
 
 # run the command and verify it return OK (exit value: 0)
