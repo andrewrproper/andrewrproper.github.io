a6e199066988d71a1232459243055f412c368770
Andrew Proper <andrewrproper@gmail.com>
2019-02-10 18:56:05 -0500

    remove old path: webpack.config.js
    because new path fixes problem with bundle.js not found under github pages

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,7 @@ module.exports = {
   // webpack folder's entry js - excluded from jekll's build process.
   entry: "./webpack/entry.js",
   output: {
-		// we're going to put the generated file in the assets folder so jekyll will grab it.
-		/* path: path.resolve( __dirname, 'assets/js/webpack/' ), */
+		// we're going to put the generated file a folder so jekyll will grab it.
 		path: path.resolve( __dirname, 'webpack-bundle/' ),
 		filename: "bundle.js"
   },
