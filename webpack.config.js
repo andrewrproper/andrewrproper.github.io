532c3209db242be3016c6f740ca6912e8eb93922
Andrew Proper <andrewrproper@gmail.com>
2019-02-10 18:49:44 -0500

    renamed:    assets/js/webpack/bundle.js -> webpack-bundle/bundle.js
    modified:   _includes/_head.html
    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
   entry: "./webpack/entry.js",
   output: {
 		// we're going to put the generated file in the assets folder so jekyll will grab it.
-		path: path.resolve( __dirname, 'assets/js/webpack/' ),
+		/* path: path.resolve( __dirname, 'assets/js/webpack/' ), */
+		path: path.resolve( __dirname, 'webpack-bundle/' ),
 		filename: "bundle.js"
   },
 	plugins: [
